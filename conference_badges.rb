243c5389bbee1026157d58ce430af894c772311e
Ariela Cohen <arielaades@gmail.com>
2019-07-22 18:29:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def badge_maker(name_array)
+  array_messages = []
+ name_array.each do |name|
+   array_messages.push "Hello, my name is #{name}."
+ end
+ return array_messages
+end

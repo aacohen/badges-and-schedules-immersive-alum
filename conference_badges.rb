00b42a937c757d3f9bbfbc895ca65c73b11dd939
Ariela Cohen <arielaades@gmail.com>
2019-07-22 18:29:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,11 @@
-def badge_maker(name_array)
+def batch_badge_creator(name_array)
   array_messages = []
  name_array.each do |name|
    array_messages.push "Hello, my name is #{name}."
  end
  return array_messages
+end
+
+def assign_rooms(speakers)
+speakers.each_with_index do |speaker,index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
 end
